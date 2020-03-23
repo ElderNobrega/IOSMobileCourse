666d835e5bbbe7fe07d189ed0651a8d71234c562
Elder Guerra <55459790+ElderNobrega@users.noreply.github.com>
2020-03-23 09:33:41 -0400

    .
    .

--- 23-02-2020-IOS.swift
+++ 23-02-2020-IOS.swift
@@ -1,1 +1,59 @@
+let alert = UIAlertController(title: "Did you bring your towel?", message: "It`s recommended you bring your towel before continuing", 
+                              preferredStyle: .alert)
+
+alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: ni1))
+alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: ni1))
+
+self.present(alert, animated: true)
+
+@IBAction func buttonTapped(_sender: UIButton) {
+  let alert = UIAlertController(title: "Warning", message:
+                                "Zombies are loose!", preferredStyle: .alert)
+  let okAction = UIAlertAction(title: "Ok", style: .default, handler: {action -> void in})
+  alert.addAction(okAction)
+  self.present(alert, animated: true, completion: nil)
+}
+
+override func viewDidLoad() {
+super.viewDidLoad()
+labelResult.numberOfLines = 0
+//Do any additional setup after loading the view, typically from a nib.
+}
+
+@IBAction func buttonTapped(_ sender: UIButton) {
+let alert = UIAlertController(title: "Warning", message:
+"Zombies are loose!", preferredStyle: .alert)
+let okAction = UIAlertAction(title: "OK", style: .default,
+handler: { action -> Void in
+self.labelResult.text = "OK"
+})
+let cancelAction = UIAlertAction(title: "Cancel", style:
+.cancel, handler: { action -> Void in
+self.labelResult.text = "Cancel"
+})
+let destroyAction = UIAlertAction(title: "Destroy", style:
+.destructive, handler: { action -> Void in
+self.labelResult.text = "Destroy"
+})
+alert.addAction(okAction)
+alert.addAction(cancelAction)
+alert.addAction(destroyAction)
+self.present(alert, animated: true, completion: nil)
+}
+
+let destroyAction = UIAlertAction(title: "Destroy", style:.destructive, handler: { action -> Void in
+self.labelResult.text = "Destroy"
+self.callFunctionOne()
+self.callFunctionTwo()
+})
+
+func callFunctionOne(){
+// Code here
+}
+
+func callFunctionTwo(){
+// Code here
+}
+
+
 
